899e931d556a75a867e1a47d424d116ff018d77f
NoNaim95 <abraun006@gmail.com>
2022-09-23 20:22:59 +0200

    Initial commit

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(doom-molokai))
+ '(custom-safe-themes
+   '("be84a2e5c70f991051d4aaf0f049fa11c172e5d784727e0b525565bb1533ec78" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "b99e334a4019a2caa71e1d6445fc346c6f074a05fcbb989800ecbe54474ae1b0" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "a19718e4958b7ed439294865bc9db9b51d9b822ed157c85fc4f7d72b6a2c6651" "6c4c97a17fc7b6c8127df77252b2d694b74e917bab167e7d3b53c769a6abb6d6" default))
+ '(package-selected-packages
+   '(phi-autopair vscode-dark-plus-theme vs-dark-theme spacemacs-theme doom-themes perspective treemacs-projectile projectile rustic company which-key lsp-mode consult treemacs-evil winum treemacs marginalia orderless vertico rainbow-mode evil use-package))
+ '(warning-suppress-types '((auto-save) (auto-save) (auto-save) (auto-save))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#181a26" :foreground "gray80" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "2ttf" :family "Hack")))))
